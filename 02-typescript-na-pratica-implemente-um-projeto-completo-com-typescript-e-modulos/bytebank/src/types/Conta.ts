a4a0a1d3ea907c47e787be3494de878a5deb133b
Alberto Lima <alberto.btlima@outlook.com>
2024-06-21 16:37:22 -0300

    feat: try, catch

--- 02-typescript-na-pratica-implemente-um-projeto-completo-com-typescript-e-modulos/bytebank/src/types/Conta.ts
+++ 02-typescript-na-pratica-implemente-um-projeto-completo-com-typescript-e-modulos/bytebank/src/types/Conta.ts
@@ -3,6 +3,25 @@ import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 3000;
 
+function debitar(valor: number): void {
+   if (valor <= 0) {
+      throw new Error("O valor a ser debitado deve ser maior do que zero!");
+   }
+   if (valor > saldo) {
+      throw new Error("Saldo insuficiente!");
+   }
+
+   saldo -= valor;
+}
+
+function depositar(valor: number): void {
+   if (valor <= 0) {
+      throw new Error("O valor a ser depositado deve ser maior do que zero!");
+   }
+
+   saldo += valor;
+}
+
 const Conta = {
    getSaldo() {
       return saldo;
@@ -12,15 +31,14 @@ const Conta = {
    },
    registrarTransacao(novaTransacao: Transacao): void {
       if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-         saldo += novaTransacao.valor;
+         depositar(novaTransacao.valor);
       } else if (
          novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
          novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
       ) {
-         saldo -= novaTransacao.valor;
+         debitar(novaTransacao.valor);
       } else {
-         alert("Tipo de transação inválido");
-         return;
+         throw new Error("Tipo de transação é inválido!");
       }
    },
 };
