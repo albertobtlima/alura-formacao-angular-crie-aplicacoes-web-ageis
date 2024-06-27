12e4e818ae6d39903dd4b19b0962720fd6eac4b5
Alberto Lima <alberto.btlima@outlook.com>
2024-06-27 14:55:16 -0300

    add search

--- 04-angular-construa-uma-aplicacao-web-com-componentes-linguagem-de-templete-e-cli/indexa/src/app/app.component.ts
+++ 04-angular-construa-uma-aplicacao-web-com-componentes-linguagem-de-templete-e-cli/indexa/src/app/app.component.ts
@@ -36,8 +36,20 @@ export class AppComponent {
 
 	filtroPorTexto: string = '';
 
-	filtrarContatosPorLetraInicial(letra: string): Contato[] {
+	filtrarContatosPorTexto(): Contato[] {
+		if (!this.filtroPorTexto) {
+			return this.contatos;
+		}
+
 		return this.contatos.filter((contato) => {
+			return contato.nome
+				.toLowerCase()
+				.includes(this.filtroPorTexto.toLowerCase());
+		});
+	}
+
+	filtrarContatosPorLetraInicial(letra: string): Contato[] {
+		return this.filtrarContatosPorTexto().filter((contato) => {
 			return contato.nome.toLowerCase().startsWith(letra);
 		});
 	}
