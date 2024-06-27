cb030050b72c22db3a4f6cb88dbc4643f7dd114a
Alberto Lima <alberto.btlima@outlook.com>
2024-06-27 17:37:28 -0300

    add contato.component.html

--- 05-angular-evoluindo-com-formularios-e-roteamento/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ 05-angular-evoluindo-com-formularios-e-roteamento/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
 
 @Component({
-  selector: 'app-formulario-contato',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './formulario-contato.component.html',
-  styleUrl: './formulario-contato.component.css'
+	selector: 'app-formulario-contato',
+	standalone: true,
+	imports: [CommonModule, ContainerComponent, SeparadorComponent],
+	templateUrl: './formulario-contato.component.html',
+	styleUrl: './formulario-contato.component.css',
 })
-export class FormularioContatoComponent {
-
-}
+export class FormularioContatoComponent {}
