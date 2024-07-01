faf3204011bc8abe8fc31e7158a4ae5880b3b173
Alberto Lima <alberto.btlima@outlook.com>
2024-07-01 12:53:46 -0300

    add valid

--- 05-angular-evoluindo-com-formularios-e-roteamento/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ 05-angular-evoluindo-com-formularios-e-roteamento/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
 
 @Component({
 	selector: 'app-formulario-contato',
@@ -21,9 +26,9 @@ export class FormularioContatoComponent {
 
 	constructor() {
 		this.contatoForm = new FormGroup({
-			nome: new FormControl('Beto'),
-			telefone: new FormControl(''),
-			email: new FormControl(''),
+			nome: new FormControl('', Validators.required),
+			telefone: new FormControl('', Validators.required),
+			email: new FormControl('', [Validators.required, Validators.email]),
 			aniversario: new FormControl(''),
 			redes: new FormControl(''),
 			observacoes: new FormControl(''),
@@ -31,7 +36,9 @@ export class FormularioContatoComponent {
 	}
 
 	salvarContato() {
-		console.log(this.contatoForm.value);
+		if (this.contatoForm.valid) {
+			console.log(this.contatoForm.value);
+		}
 	}
 
 	cancelar() {
