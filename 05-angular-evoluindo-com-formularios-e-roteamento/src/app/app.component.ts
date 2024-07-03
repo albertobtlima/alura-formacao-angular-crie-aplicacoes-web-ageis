1da67a3cdafc2db3c256e20f0e31ce4a2a6942ae
Alberto Lima <alberto.btlima@outlook.com>
2024-07-03 16:03:47 -0300

    add app routes

--- 05-angular-evoluindo-com-formularios-e-roteamento/src/app/app.component.ts
+++ 05-angular-evoluindo-com-formularios-e-roteamento/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
 import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
 import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
@@ -10,9 +12,9 @@ import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.
 		CommonModule,
 		FormularioContatoComponent,
 		ListaContatosComponent,
+		RouterOutlet,
 	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
-
 export class AppComponent {}
