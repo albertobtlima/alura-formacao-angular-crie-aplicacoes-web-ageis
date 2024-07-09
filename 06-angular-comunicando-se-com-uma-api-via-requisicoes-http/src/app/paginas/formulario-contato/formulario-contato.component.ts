60dfcb2080a93354ff438b49dbf8297da18752d6
Alberto Lima <alberto.btlima@outlook.com>
2024-07-09 18:37:42 -0300

    add function editar contato

--- 06-angular-comunicando-se-com-uma-api-via-requisicoes-http/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ 06-angular-comunicando-se-com-uma-api-via-requisicoes-http/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
 } from '@angular/forms';
 
 import { ContainerComponent } from '../../componentes/container/container.component';
@@ -13,47 +13,64 @@ import { SeparadorComponent } from '../../componentes/separador/separador.compon
 import { ContatoService } from '../../services/contato.service';
 
 @Component({
-  selector: 'app-formulario-contato',
-  standalone: true,
-  imports: [
-    CommonModule,
-    ContainerComponent,
-    SeparadorComponent,
-    ReactiveFormsModule,
-    RouterLink,
-  ],
-  templateUrl: './formulario-contato.component.html',
-  styleUrl: './formulario-contato.component.css',
+	selector: 'app-formulario-contato',
+	standalone: true,
+	imports: [
+		CommonModule,
+		ContainerComponent,
+		SeparadorComponent,
+		ReactiveFormsModule,
+		RouterLink,
+	],
+	templateUrl: './formulario-contato.component.html',
+	styleUrl: './formulario-contato.component.css',
 })
 export class FormularioContatoComponent implements OnInit {
-  contatoForm!: FormGroup;
+	contatoForm!: FormGroup;
 
-  constructor(private contatoService: ContatoService, private router: Router) {}
+	constructor(
+		private contatoService: ContatoService,
+		private router: Router,
+		private activatedRouter: ActivatedRoute
+	) {}
 
-  ngOnInit() {
-    this.inicializarFormulario();
-  }
+	ngOnInit() {
+		this.inicializarFormulario();
+    this.carregarContato();
+	}
 
-  inicializarFormulario() {
-    this.contatoForm = new FormGroup({
-      nome: new FormControl('', Validators.required),
-      telefone: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      aniversario: new FormControl(''),
-      redes: new FormControl(''),
-      observacoes: new FormControl(''),
-    });
-  }
+	inicializarFormulario() {
+		this.contatoForm = new FormGroup({
+			nome: new FormControl('', Validators.required),
+			telefone: new FormControl('', Validators.required),
+			email: new FormControl('', [Validators.required, Validators.email]),
+			aniversario: new FormControl(''),
+			redes: new FormControl(''),
+			observacoes: new FormControl(''),
+		});
+	}
 
-  salvarContato() {
-    const novoContato = this.contatoForm.value;
-    this.contatoService.salvarContato(novoContato).subscribe(() => {
-      this.contatoForm.reset();
-      this.router.navigateByUrl('/lista-contatos');
-    });
-  }
+	carregarContato() {
+		const id = this.activatedRouter.snapshot.paramMap.get('id');
+		if (id) {
+			this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+				this.contatoForm.patchValue(contato);
+			});
+		}
+	}
 
-  cancelar() {
-    this.contatoForm.reset();
-  }
+	salvarContato() {
+		const novoContato = this.contatoForm.value;
+		const id = this.activatedRouter.snapshot.paramMap.get('id');
+		novoContato.id = id ? parseInt(id) : null;
+
+		this.contatoService.editarOuSalvarContato(novoContato).subscribe(() => {
+			this.contatoForm.reset();
+			this.router.navigateByUrl('/lista-contatos');
+		});
+	}
+
+	cancelar() {
+		this.contatoForm.reset();
+	}
 }
